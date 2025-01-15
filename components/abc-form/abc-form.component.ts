4fa0d02d1fc663c80ffbd9a5b3de4538925fcf6e
piotrrek <irache@wp.pl>
2025-01-15 10:57:32 +0100

    fixes

--- components/abc-form/abc-form.component.ts
+++ components/abc-form/abc-form.component.ts
@@ -1,44 +1,42 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder } from '@angular/forms';
-import { BaseFormComponent } from '@shared/components/base-form.component';
-import { AbcForm } from '@shared/forms/abc.form';
-import { AbcData } from './abc-data.interface';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { AbstractControl, FormArray, FormBuilder } from "@angular/forms";
+import { BaseFormComponent } from "@shared/components/base-form.component";
+import { AbcForm } from "@shared/forms/abc.form";
+import { AbcData } from "./abc-data.interface";
 
 @Component({
-     selector: 'app-form-abc',
-     templateUrl: './abc-form.component.html',
-     styleUrls: ['./abc-form.component.scss'],
+  selector: "app-form-abc",
+  templateUrl: "./abc-form.component.html",
+  styleUrls: ["./abc-form.component.scss"],
 })
 export class AbcFormComponent extends BaseFormComponent implements OnInit {
-     @Input() public set nrOfRows(nrOfRows: number) {
-          // nrOfRows ?
-          this.addFormItems();
-          // : null;
-     }
+  @Input() public set nrOfRows(nrOfRows: number) {
+    // nrOfRows ?
+    this.addFormItems();
+    // : null;
+  }
 
-     @Output() public formChange = new EventEmitter<AbcData[]>();
-     @Output() public isFormValid = new EventEmitter<boolean>();
+  @Output() public formChange = new EventEmitter<AbcData[]>();
+  @Output() public isFormValid = new EventEmitter<boolean>();
 
-     public form: AbcForm = new AbcForm(this.fb);
-     public items: FormArray = this.form.itemsArray;
+  public form: AbcForm = new AbcForm(this.fb);
+  public items: FormArray = this.form.itemsArray;
 
-     constructor(private fb: FormBuilder) {
-          super();
-     }
+  constructor(private fb: FormBuilder) {
+    super();
+  }
 
-     public ngOnInit(): void {
-          this.checkFormAndEmit();
-     }
+  public ngOnInit(): void {
+    this.checkFormAndEmit();
+  }
 
-     public addFormItems(): void {
-          console.log('addItem');
-          // this.items.clear();
-          // for (let i = 0; i <= nrOfRows; i++) {
-          this.items.push(this.form.createForm());
-          // }
-     }
+  public addFormItems(): void {
+    console.log("addItem");
 
-     public getItemsControls(): AbstractControl[] {
-          return this.form.getItemsControls();
-     }
+    this.items.push(this.form.createForm());
+  }
+
+  public getItemsControls(): AbstractControl[] {
+    return this.form.getItemsControls();
+  }
 }
